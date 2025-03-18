c410a841b774819f7a06b3f5801631078d9fbaea
Coaffy <137829876+Coaffy@users.noreply.github.com>
2025-03-18 17:14:49 -0300

    Update super_calculadora.py

--- super_calculadora.py
+++ super_calculadora.py
@@ -5,5 +5,5 @@ print("Este código siempre se ejecuta.")
 
 if __name__ == "__main__":
     print("Ejecutando como script principal.")
-    resultado = sumar(2, 3)
+    resultado = sumar(5, 4)
     print(f"Resultado: {resultado}")
