d7e3bb9a80452c9dbb39446e7404ed0cb40d93d2
Paulina <paulinagpyl@gmail.com>
2025-03-18 16:42:00 -0300

    first commit

---
+++ otro_programa.py
@@ -0,0 +1,4 @@
+import super_calculadora
+
+print("Usando la calculadora en otro programa.")
+print(super_calculadora.sumar(4, 5))
---
+++ super_calculadora.py
@@ -0,0 +1,9 @@
+def sumar(a, b):
+    return a + b
+
+print("Este código siempre se ejecuta.")
+
+if __name__ == "__main__":
+    print("Ejecutando como script principal.")
+    resultado = sumar(2, 3)
+    print(f"Resultado: {resultado}")
